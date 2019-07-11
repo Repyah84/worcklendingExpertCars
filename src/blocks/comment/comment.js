0f246c680fd0fd83a7f1d112ee682b0794eb5f3b
Nikolay Nikolay <you@example.com>
2019-07-11 17:22:54 +0300

    add chenge js

--- src/blocks/comment/comment.js
+++ src/blocks/comment/comment.js
@@ -13,13 +13,13 @@ export default function(){
             disableOnInteraction: false,
         },
         
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-            renderBullet: function (index, className) {
-              return '<span class="' + className + '">' + (index + 1) + '</span>';
-            },
-        },
+        // pagination: {
+        //     el: '.swiper-pagination',
+        //     clickable: true,
+        //     renderBullet: function (index, className) {
+        //       return '<span class="' + className + '">' + (index + 1) + '</span>';
+        //     },
+        // },
 
         // breakpointsInverse: true,
         breakpoints: {
